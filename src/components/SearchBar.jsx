85d7d9bb8fa6a31d56f6a0cf1cd06ceaa05a1619
Laassel <lassel.giovanni@gmail.com>
2025-06-24 16:28:01 +0200

    feat: add debounce to optimize search input performance

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { BASE_URL } from "../config/api";
+import debounce from "../utils/debounce";
 
 function SearchBar () {
     const [searchTerm, setSearchTerm] = useState ([]);
     const [results, setResults] = useState ([]);
+
+    function fetchData(term) {
+            fetch(`${BASE_URL}/products?search=${searchTerm}`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("API Response", data);
+                setResults(data);
+            });
+    }
+
+    const debouncedFetch = useCallback(
+        debounce(fetchData, 3000),
+        []
+    );
 
     useEffect (() => {
         if(searchTerm === ""){
@@ -11,16 +26,8 @@ function SearchBar () {
             return;
         }
 
-        fetch(`${BASE_URL}/products?search=${searchTerm}`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log("API Response", data);
-            setResults(data);
-        });
-
-    }, [searchTerm])
-
-
+        debouncedFetch(searchTerm)
+    }, [searchTerm, debouncedFetch])
 
     return (
         <div>
---
+++ src/utils/debounce.js
@@ -0,0 +1,13 @@
+function debounce (fn, delay) {
+    let timeout;
+
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            fn(...args);
+            
+        }, delay);
+    }
+}
+
+export default debounce;
